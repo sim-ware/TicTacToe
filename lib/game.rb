0fcd9213334349da21503aa66f16294aebdd12d1
Samir Gossain <samir.gossain@gmail.com>
2017-07-24 17:56:15 +0100

    Done for the Day

--- lib/game.rb
+++ lib/game.rb
@@ -13,4 +13,25 @@ class Game
   def solicit_move
     "#{current_player.name}; Please Enter a no. between 1 & 9 to make your move"
   end
+
+  def get move(human_move = gets.chomp)
+    human_move_to_coordinate(human_move)
+  end
+
+  private
+
+  def human_move_to_coordinate(human_move)
+    mapping = {
+      "1" => [0, 0],
+      "2" => [1, 0],
+      "3" => [2, 0],
+      "4" => [0, 1],
+      "5" => [1, 1],
+      "6" => [2, 1],
+      "7" => [0, 2],
+      "8" => [1, 2],
+      "9" => [2, 2]
+    }
+    mapping[human_move]
+  end
 end
