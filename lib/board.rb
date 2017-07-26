3091704cbe6197024ee7370aea060ad36457f3b8
Samir Gossain <samir.gossain@gmail.com>
2017-07-26 12:07:19 +0100

    Adds #formatted_grid method to board.rb, needs tests

--- lib/board.rb
+++ lib/board.rb
@@ -19,19 +19,17 @@ class Board
     false
   end
 
-  def winning_positions
-    grid + # rows
-    grid.transpose + # columns
-    diagonals # two diagonals
+  def formatted_grid
+    grid.each do |row|
+      puts row.map { |cell| cell.value.empty? ? "_" : cell.value }.join(" ")
+    end
   end
 
-  def diagonals
-  [
-    [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2)],
-    [get_cell(0, 2), get_cell(1, 1), get_cell(2, 0)]
-  ]
-  end
+  private
 
+  def default_grid
+    Array.new(3) { Array.new(3) { Cell.new } }
+  end
 
   def draw?
     grid.flatten.map { |cell| cell.value }.none_empty?
@@ -49,10 +47,17 @@ class Board
     winning_position.map { |cell| cell.value }
   end
 
-  private
+  def winning_positions
+    grid + # rows
+    grid.transpose + # columns
+    diagonals # two diagonals
+  end
 
-  def default_grid
-    Array.new(3) { Array.new(3) { Cell.new } }
+  def diagonals
+  [
+    [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2)],
+    [get_cell(0, 2), get_cell(1, 1), get_cell(2, 0)]
+  ]
   end
 
 end
